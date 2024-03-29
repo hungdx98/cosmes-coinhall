b0469ea33980cc66568b206cacb46b2a1fd53cec
Đinh Xuân Hùng <dxhung@coin98.finance>
2024-03-29 10:36:46 +0700

    fix tsx

--- examples/solid-vite/vite.config.ts
+++ examples/solid-vite/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
+      entry: path.resolve(__dirname, 'index.tsx'),
       formats: ['es'],
       fileName: () => 'out.js',
     },
